01b7ae354a64319f33cc22a5566dedb272635ba6
OnanThairu <thairuonan@gmail.com>
2022-10-04 18:13:21 +0300

    Add Posts Serializer.

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :short_content
+  attributes :title, :content
 
-  def short_content
-    "#{self.object.content[0..39]}..."
-  end
+  belongs_to :author
+  has_many :tags
+
 end
